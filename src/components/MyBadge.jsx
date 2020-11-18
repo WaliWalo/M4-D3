e5ca3434e208053d2aad9fae2ee420377ddf3ade
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2020-11-18 15:54:01 +0100

    Excercise  6

--- src/components/MyBadge.jsx
+++ src/components/MyBadge.jsx
@@ -3,7 +3,7 @@ import { Badge } from "react-bootstrap";
 
 const MyBadge = (props) => {
   let style = {
-    marginLeft: '20px;'
+    marginLeft: '20px'
   }
   return (
     <Badge variant={props.color} style={style}>{props.category}</Badge>
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -3,26 +3,48 @@ import { Card, Button } from "react-bootstrap";
 
 import MyBadge from './MyBadge.jsx';
 
-export default function SingleBook(prop) {
+class SingleBook extends React.Component{
 
-  let variant;
-  if(prop.book.category === 'fantasy')
-    variant = 'info';
-  else if(prop.book.category === 'history')
-    variant = 'warning'
-  else if(prop.book.category === 'horror')
-    variant = 'dark'
-  else variant = 'light'
+  state = {
+    selected: false
+  }
+  
+  render(){
 
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={prop.book.img} />
-      <Card.Body>
-        <Card.Title>{prop.book.title}</Card.Title>
-        <Card.Text>{prop.book.price}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-        <MyBadge color={variant} category={prop.book.category}/>
-      </Card.Body>
-    </Card>
-  );
+    let variant;
+    if(this.props.book.category === 'fantasy')
+      variant = 'info';
+    else if(this.props.book.category === 'history')
+      variant = 'warning'
+    else if(this.props.book.category === 'horror')
+      variant = 'dark'
+    else variant = 'light'
+
+    let handleClick = () => {
+      this.setState({selected: !this.state.selected});
+    }
+
+    let cardStyle; 
+
+    if(this.state.selected)
+      cardStyle = {
+        boxShadow: '2px 4px solid #444',
+        color: '#bbb'
+      }
+    else cardStyle = {}
+
+    return (
+      <Card style={{ width: "18rem" }} onClick={handleClick}>
+        <Card.Img variant="top" src={this.props.book.img} className={cardStyle} />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>{this.props.book.price}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+          <MyBadge color={variant} category={this.props.book.category}/>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
+
+export default SingleBook;
